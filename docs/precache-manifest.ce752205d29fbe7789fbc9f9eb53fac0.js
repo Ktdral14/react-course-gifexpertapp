8895c2b2a7dc0adf3766465594860d9a4e0053e8
Ktdral14 <angel.catedral.1410@gmail.com>
2020-08-23 20:41:41 -0500

    Docs agregado

---
+++ docs/precache-manifest.ce752205d29fbe7789fbc9f9eb53fac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d74cc7a9b73d514abeff7e01f7b215",
+    "url": "/index.html"
+  },
+  {
+    "revision": "294f0b76c5f60f1c0fef",
+    "url": "/static/css/main.0c21b1db.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "294f0b76c5f60f1c0fef",
+    "url": "/static/js/main.b517a911.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
