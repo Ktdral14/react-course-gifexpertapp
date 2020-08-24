411a6db46cf4bb202a0dafbd4adb501513b2ce52
Ktdral14 <angel.catedral.1410@gmail.com>
2020-08-23 21:01:10 -0500

    Updates

---
+++ precache-manifest.11c2e58145c818e2945273f878e9005d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2c550928a82b1d537bac6d2700512a",
+    "url": "/react-course-gifexpertapp/index.html"
+  },
+  {
+    "revision": "294f0b76c5f60f1c0fef",
+    "url": "/react-course-gifexpertapp/static/css/main.0c21b1db.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/react-course-gifexpertapp/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-course-gifexpertapp/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "294f0b76c5f60f1c0fef",
+    "url": "/react-course-gifexpertapp/static/js/main.b517a911.chunk.js"
+  },
+  {
+    "revision": "a7f95a1bd00f0d68afa6",
+    "url": "/react-course-gifexpertapp/static/js/runtime-main.10e00633.js"
+  }
+]);
